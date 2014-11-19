396e014f81a8104117586cf54866958b5a6e3dbf
compedit <troybetz@gmail.com>
2014-11-18 22:37:28 -0600

    test error handling bad responses from noembed.com

--- test/mediaEmbed-test.js
+++ test/mediaEmbed-test.js
@@ -15,12 +15,20 @@ describe('mediaEmbed', function() {
     expect(mediaEmbed).to.throw('url required for embedding');
   });
 
-  it('should retrieve embed html from noembed.com', function() {
+  it('should retrieve html for embedding from noembed.com', function() {
     mediaEmbed('https://soundcloud.com/hudsonmohawke/chimes');
     expect(jsonpStub.firstCall.args[0]).to.equal('https://noembed.com/embed?url=https://soundcloud.com/hudsonmohawke/chimes&nowrap=on');
   });
 
-  it('should append embed html to the `parent`', function() {
+  it('should throw an error if `url` is not supported by noembed.com', function() {
+    function fn() {
+      mediaEmbed('http://somebadurl.com');
+      jsonpStub.yield(null, {});
+    }
+    expect(fn).to.throw('media not supported');
+  });
+
+  it('should append the embed html to the `parent`', function() {
     var parent = document.createElement('div');
     parent.id = 'parent';
     document.body.appendChild(parent);
